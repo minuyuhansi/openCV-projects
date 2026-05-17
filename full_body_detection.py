2780a68c6fab856b66c80da9fffc4baae701a640
minuyuhansi <muthumunigesilva21@gmail.com>
2026-05-17 20:03:34 +0530

    full body detection

---
+++ full_body_detection.py
@@ -0,0 +1,15 @@
+import cv2
+
+body_director = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_fullbody.xml')
+
+img = cv2.imread("Res/fullBody.png")
+g = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+# body detect
+bodies = body_director.detectMultiScale(g, scaleFactor=1.05, minNeighbors=5)
+
+for (x, y, w, h) in bodies:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+
+cv2.imshow("Full Body Detection",img)
+cv2.waitKey(0)
